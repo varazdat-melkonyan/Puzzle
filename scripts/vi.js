09128bb92f5777a478e01adf09dfdce2170641b2
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-12-14 19:12:39 +0400

    Update vi.js

--- scripts/vi.js
+++ scripts/vi.js
@@ -1,9 +1,12 @@
 const vview = {
     correct: 0,
     row: `<div class="row"></div>`,
-    addPuzzle: async (parent, i, text) => {
-            $(parent).append(`<div class="left ${i}"><p>${text}</p></div>`);
-            $(parent).append(`<div class="center ${i}"><p>${text}</p></div>`);
-            $(parent).append(`<div class="right ${i}"><p>${text}</p></div>`);
+    addPuzzle: async (firstText, secondText, threeText) => {
+            $(".left").append(`<p>${firstText}</p>`);
+            $(".center").append(`<p>${secondText}</p>`);
+            $(".right").append(`<p>${threeText}</p>`);
+    },
+    editPuzzle: (i, firstText, secondText, threeText) => {
+        
     }
 }
